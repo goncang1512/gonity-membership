fe345bae50f5376771d5fe6ec347e0d0b1c30e73
goncang1512 <samuderanst@gmail.com>
2025-08-11 18:42:51 +0700

    feat: add cors allowed api

--- src/services/version_1/version_1.ts
+++ src/services/version_1/version_1.ts
@@ -1,10 +1,13 @@
 import { Hono } from "hono";
 import memberApp from "./membership";
 import { verifyApi } from "@/src/middleware/verify-key";
+import { allowOrigins } from "@/src/middleware/allow-origins";
 
 const app = new Hono();
 
 app.use("/*", verifyApi);
+// CORS middleware async
+app.use("/*", allowOrigins);
 
 app.route("/membership", memberApp);
 
