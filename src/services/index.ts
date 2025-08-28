343be61e7ac03203f2f08f7514963fae0c2031fc
goncang1512 <samuderanst@gmail.com>
2025-08-28 11:27:06 +0700

    fix: bug in deploy to vercel app

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,7 +3,8 @@ import { Hono } from "hono";
 import authApp from "./auth";
 import versionOne from "./version_1/version_1";
 
-// export const runtime = "edge";
+export const runtime = "edge";
+
 const app = new Hono().basePath("/api");
 
 const routes = app.route("/auth", authApp).route("/v1", versionOne);
