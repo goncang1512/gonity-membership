4139dcc22beae1615f505b5dbf0b562e6e2833eb
goncang1512 <samuderanst@gmail.com>
2025-08-28 11:40:10 +0700

    fix: bug deployment

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,8 +3,6 @@ import { Hono } from "hono";
 import authApp from "./auth";
 import versionOne from "./version_1/version_1";
 
-export const runtime = "edge";
-
 const app = new Hono().basePath("/api");
 
 const routes = app.route("/auth", authApp).route("/v1", versionOne);
